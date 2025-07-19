6765f7874ab2083a6f5b8cf5c58ba02c461c47a2
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-19 11:40:44 +0300

    Add "Remove Element (O)" and "Remove Element (BF)" projects
    - Updated `Data Structures & Algorithms.sln` to include new projects and remove the old "Remove Element" project.
    - Configured global settings for the new "Remove Element (BF)" project for Debug and Release modes across x64 and Win32 platforms.
    - Modified `Remove Element.vcxproj` to reflect the new project name.
    - Added `Brute Force.cpp` with a brute force algorithm for element removal.
    - Created `Remove Element (BF).vcxproj` and `Remove Element (BF).vcxproj.filters` to define and organize the new project structure.

---
+++ Remove Element (BF)/Brute Force.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int k = n;
+        for (int i = 0; i < k;i++) {
+            if (nums[i] == val) {
+                for (int j = i + 1;j < k;j++) {
+                    nums[j - 1] = nums[j];
+                }
+                k--;
+                i--;
+            }
+        }
+
+        return k;
+    }
+};
