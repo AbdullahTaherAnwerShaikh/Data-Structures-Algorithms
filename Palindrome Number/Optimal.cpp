e59ae9477a6115523d7c877db828e77216eae1f2
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-20 12:17:47 +0300

    Add Palindrome Number project and implementation
    - Introduced a new project "Palindrome Number" in the solution.
    - Created `Optimal.cpp` with a class `Solution` to check for palindromes.
    - Added project configuration file `Palindrome Number.vcxproj`.
    - Created filters file `Palindrome Number.vcxproj.filters` for organization.

---
+++ Palindrome Number/Optimal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))return false;
+
+
+        long long rev = 0;
+        long long num = x;
+        while (rev < num) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        return (rev == num || rev / 10 == num);
+    }
+};
