17695dc77f6a5562cd4e2f413e214110705c90f7
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-21 11:08:02 +0300

    Add "Merge Sorted Array" project to solution
    This commit introduces a new project named "Merge Sorted Array" with its associated project and filter files. The project supports Win32 and x64 configurations for both Debug and Release modes. It includes a source file `Optimal.cpp`, which contains a `Solution` class with a method to merge and sort two arrays. The solution configuration has been updated to recognize the new project, and the filter file organizes project files into appropriate categories.

---
+++ Merge Sorted Array/Optimal.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int j = nums2.size() - 1;
+        for (int i = nums1.size() - 1; j >= 0; i--) {
+            nums1[i] = nums2[j];
+            j--;
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
