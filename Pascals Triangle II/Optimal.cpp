8b28fce92ac87558370612fce6ee5a3eb591686b
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-22 11:32:34 +0300

    Fix Pascal's triangle row generation logic
    Updated loop condition to include the specified row index.
    Resized the vector at the beginning of each iteration and
    combined assignments for clarity. Improved initialization
    and resizing for correctness and efficiency.

--- Pascals Triangle II/Optimal.cpp
+++ Pascals Triangle II/Optimal.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int>row(1, 1);
-        for (int i = 1;i < rowIndex;i++) {
-            row[0] = 1;
-            row[i] = 1;
-            for (j = i - 1;j > 0;j--) {
+        vector<int>row(1, 1);  //Creates a vector row of size 1, with the value 1.
+        for (int i = 1;i <= rowIndex;i++) {
+            row.resize(i + 1);
+            row[i] = row[0] = 1;
+            for (int j = i - 1;j > 0;j--) {
                 row[j] = row[j] + row[j - 1];
             }
         }
