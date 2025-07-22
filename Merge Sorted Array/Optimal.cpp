ec33be13d317902e8b224b44417f53eb95e177fd
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-22 11:33:33 +0300

    Fix index initialization for merging arrays
    Updated the initialization of variable `j` from `nums2.size() - 1` to `n - 1` to accurately reflect the last index of `nums2`. This change enhances code readability and correctness by ensuring `j` corresponds to the provided size parameter instead of the actual size of `nums2`.

--- Merge Sorted Array/Optimal.cpp
+++ Merge Sorted Array/Optimal.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j = nums2.size() - 1;
-        for (int i = nums1.size() - 1; j >= 0; i--) {
+        int j = n - 1;
+        for (int i = nums1.size() - 1;j >= 0;i--) {
             nums1[i] = nums2[j];
             j--;
         }
