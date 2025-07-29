604effe6b72173624acddd3d71a32dc10b36f032
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-29 11:34:17 +0300

    Revision of Jump Game.

--- Jump Game/Optimal.cpp
+++ Jump Game/Optimal.cpp
@@ -3,12 +3,10 @@ public:
     bool canJump(vector<int>& nums) {
         int maxreach = 0;
         for (int i = 0; i < nums.size();i++) {
-            if (i > maxreach) {
-                return false;
-            }
+            if (i > maxreach) return false;
             maxreach = max(maxreach, i + nums[i]);
-            if (maxreach >= nums.size() - 1) { return true; }
+            if (maxreach >= nums.size() - 1)  return true; 
         }
-        return true;
+        return 0;
     }
 };
