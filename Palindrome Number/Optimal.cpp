a7f23f2387680d596acf9736e2792ba289b53606
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-29 11:43:26 +0300

    Revision of palindrome number

--- Palindrome Number/Optimal.cpp
+++ Palindrome Number/Optimal.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0))return false;
-
-
         long long rev = 0;
         long long num = x;
         while (rev < num) {
