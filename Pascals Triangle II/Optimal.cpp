dd4fe1acff9c8da7919ee6f133c36c8c99423277
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-29 14:12:42 +0300

    Revision of Pascals Triangle II

--- Pascals Triangle II/Optimal.cpp
+++ Pascals Triangle II/Optimal.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int>row(1, 1);  //Creates a vector row of size 1, with the value 1.
+        vector<int> row(1, 1);
         for (int i = 1;i <= rowIndex;i++) {
             row.resize(i + 1);
-            row[i] = row[0] = 1;
+            row[0] = row[i] = 1;
             for (int j = i - 1;j > 0;j--) {
                 row[j] = row[j] + row[j - 1];
             }
