13a6857cad3e78bece47836f4c27220e7bd38b1c
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-29 12:49:16 +0300

    Revision of Merge sorted array

--- Merge Sorted Array/Optimal.cpp
+++ Merge Sorted Array/Optimal.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j = n - 1;
+        int j = nums2.size() - 1;
         for (int i = nums1.size() - 1;j >= 0;i--) {
             nums1[i] = nums2[j];
             j--;
