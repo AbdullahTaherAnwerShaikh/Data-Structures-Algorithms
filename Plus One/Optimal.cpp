50131a713298214f193b30534806888ba0aaa3eb
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-29 11:28:38 +0300

    Revision of plus one

--- Plus One/Optimal.cpp
+++ Plus One/Optimal.cpp
@@ -4,7 +4,7 @@ public:
 
         for (int i = digits.size() - 1;i >= 0;i--) {
             if (digits[i] < 9) {
-                digits[i]++;
+                digits[i]+=1;
                 return digits;
             }
             else {
