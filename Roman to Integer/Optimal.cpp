ba571f935e37addfa6f3912fc1e10e5ff7a7bcb0
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-07-29 12:10:16 +0300

    Revision of Roman to Integer

--- Roman to Integer/Optimal.cpp
+++ Roman to Integer/Optimal.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))return false;
-
-
-        long long rev = 0;
-        long long num = x;
-        while (rev < num) {
-            rev = rev * 10 + num % 10;
-            num = num / 10;
+    int char2num(char a) {
+        switch (a) {
+        case 'I':return 1;
+        case 'V':return 5;
+        case 'X':return 10;
+        case 'L':return 50;
+        case 'C':return 100;
+        case 'D':return 500;
+        case 'M':return 1000;
+        default:return 0;
         }
-        return (rev == num || rev / 10 == num);
+    }
+    int romanToInt(string s) {
+        int result = 0;
+        for (int i = 0;i < s.length();i++) {
+            if (i + 1 < s.length() && char2num(s[i]) < char2num(s[i + 1])) {
+                result -= char2num(s[i]);
+            }
+            else {
+                result += char2num(s[i]);
+            }
+        }
+        return result;
     }
 };
