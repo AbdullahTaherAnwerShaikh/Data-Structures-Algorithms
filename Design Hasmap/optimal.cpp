a6e50e0ad3cdf3757f2cbd31e2655776678f54b8
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-08-16 12:33:52 +0300

    Design Hashmap

---
+++ Design Hasmap/optimal.cpp
@@ -0,0 +1,30 @@
+class MyHashMap {
+public:
+    unordered_map<int, int> map;
+    MyHashMap() {
+
+    }
+
+    void put(int key, int value) {
+        map[key] = value;
+    }
+
+    int get(int key) {
+        if (map.find(key) != map.end()) {
+            return map[key];
+        }
+        return -1;
+    }
+
+    void remove(int key) {
+        map[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
