f7ed141a0889610fa3ab9dbc7e70b504d42f90d8
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-08-20 13:38:48 +0300

    First Missing Positive (BF)

---
+++ First Missing Positive (BF)/brute force.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int missing = 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == missing) {
+                missing++;
+            }
+        }
+        return missing;
+    }
+};
