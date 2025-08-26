daf61aa92616a8a040b480d30b569e7393e03035
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-08-26 14:20:16 +0300

    Trapping Rain Water

---
+++ Trapping Rain Water/optimal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int i = 0, leftMax = height[i], sum = 0;
+        int j = height.size() - 1, rightMax = height[j];
+        while (i < j) {
+            if (leftMax <= rightMax) {
+                sum += (leftMax - height[i]);
+                i++;
+                leftMax = max(leftMax, height[i]);
+            }
+            else {
+                sum += (rightMax - height[j]);
+                j--;
+                rightMax = max(rightMax, height[j]);
+            }
+        }
+        return sum;
+    }
+};
