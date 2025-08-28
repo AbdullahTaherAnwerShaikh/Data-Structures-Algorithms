1c5de2b0623a7f39c20fb27a83414d55dd310560
Abdullah Taher Anwer Shaikh <abdullah.taher810@gmail.com>
2025-08-28 12:44:30 +0300

    Minimum Size Subarray Sum

--- Minimum Size Subarray Sum/optimal.cpp
+++ Minimum Size Subarray Sum/optimal.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int start = 0;
-        int sum = 0;
-        int end = 0;
-        int minLength = INT_MAX;
+        int minLen = INT_MAX, start = 0, sum = 0;
         for (int end = 0;end < nums.size();end++) {
             sum += nums[end];
             while (sum >= target) {
-                minLength = min(minLength, end - start + 1);
+                minLen = min(minLen, end - start + 1);
                 sum -= nums[start];
                 start++;
             }
         }
-        if (minLength == INT_MAX) return 0;
-        return minLength;
+        if (minLen == INT_MAX) return 0;
+        return minLen;
     }
 };
